94318b8567c9180ac3d5d7ec3cb386522cab8e36
Ataru Nakano <neu.nakano@gmail.com>
2019-10-10 11:28:28 +0900

    Take over

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hero">
+    <picture class="hero__picture">
+      <source media="(min-width: 768px)" srcset="image/hero.jpg 1x, image/hero@2x.jpg 2x" />
+      <source media="(max-width: 767.9px)" srcset="image/hero@sp.jpg" />
+      <img class="hero__image lazyload" src="image/hero.jpg" />
+    </picture>
+    <div class="hero__content">
+      <div class="pickup">
+        <div class="pickup__swiperWrap">
+          <div class="pickup__swiper swiper-container">
+            <div class="pickup__wrapper swiper-wrapper">
+              <div class="pickup__slide swiper-slide">
+                <a class="pickup__link" href="#">
+                  <picture class="pickup__picture">
+                    <source
+                      media="(min-width: 768px)"
+                      srcset="@/assets/image/pickup01.png 1x, @/assets/image/pickup01@2x.png 2x"
+                    />
+                    <source media="(max-width: 767.9px)" srcset="@/assets/image/pickup01@sp.png" />
+                    <img
+                      class="pickup__image lazyload"
+                      src="@/assets/image/pickup01.png"
+                      alt="ご入居お祝い金キャンペーン"
+                    />
+                  </picture>
+                </a>
+              </div>
+              <div class="pickup__slide swiper-slide">
+                <a class="pickup__link" href="#">
+                  <picture class="pickup__picture">
+                    <source
+                      media="(min-width: 768px)"
+                      srcset="@/assets/image/pickup01.png 1x, @/assets/image/pickup01@2x.png 2x"
+                    />
+                    <source media="(max-width: 767.9px)" srcset="@/assets/image/pickup01@sp.png" />
+                    <img
+                      class="pickup__image lazyload"
+                      src="@/assets/image/pickup01.png"
+                      alt="ご入居お祝い金キャンペーン"
+                    />
+                  </picture>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <aside class="pickup__pagination swiper-pagination"></aside>
+        <aside class="pickup__controller">
+          <div class="pickup__prev swiper-button-prev"><?php include 'arrow.php'; ?></div>
+          <div class="pickup__next swiper-button-next"><?php include 'arrow.php'; ?></div>
+        </aside>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
